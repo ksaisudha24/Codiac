3be9d080daad668492ff59211d32d3842b1a4728
ksaisudha24 <68504200+ksaisudha24@users.noreply.github.com>
2021-01-30 10:07:58 +0530

    Removed redundant code

--- Codiac/__init__.py
+++ Codiac/__init__.py
@@ -17,10 +17,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     @app.route('/')
     def home():
         return render_template('auth/home.html')
